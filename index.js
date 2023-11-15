c539c063a1ee5765e7892e6b921bca143ef9e3b7
Roman Nuritdinov (Ky6uk) <github@woof.slmail.me>
2023-11-15 16:20:00 +0200

    Fix path issue

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const baseRules = require.resolve('./rules/base');
-const stylisticJsRules = require.resolve('./rules/stylistic/js');
+const stylisticJsRules = require.resolve('./rules/stylistic-js');
 const typescriptRules = require.resolve('./rules/typescript');
 
 module.exports = {
