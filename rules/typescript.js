3fdcb58cb800c2850ede6fd95e6c74f736c30d36
Roman Nuritdinov (Ky6uk) <github@woof.slmail.me>
2023-11-22 14:41:11 +0200

    🚀 Upgrade dependencies

--- rules/typescript.js
+++ rules/typescript.js
@@ -88,6 +88,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-enum-comparison': 'error',
     '@typescript-eslint/no-unsafe-member-access': 'error',
     '@typescript-eslint/no-unsafe-return': 'error',
+    '@typescript-eslint/no-unsafe-unary-minus': 'error',
 
     // Formatting Rules
 
