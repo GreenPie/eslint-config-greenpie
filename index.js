f7cfe7e774b125a3bd5990889eb3ffe9f7b32a23
Roman Nuritdinov (Ky6uk) <github@woof.slmail.me>
2024-02-21 16:18:15 +0200

    fixup! Support flat configs

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import baseConfig from './configs/base.js';
+import typeScriptConfig from './configs/typescript.js';
+import vueConfig from './configs/vue.js';
 import stylisticJsConfig from './configs/stylistic/js.js';
 import stylisticPlusConfig from './configs/stylistic/plus.js';
 import stylisticTsConfig from './configs/stylistic/ts.js';
-import typeScriptConfig from './configs/typescript.js';
 
-export default [
-  ...baseConfig,
-  ...stylisticJsConfig,
-  ...typeScriptConfig,
-  ...stylisticTsConfig,
-  ...stylisticPlusConfig
-];
+export default {
+  configs: {
+    base: baseConfig,
+    stylisticJs: stylisticJsConfig,
+    stylisticPlus: stylisticPlusConfig,
+    stylisticTs: stylisticTsConfig,
+    typeScript: typeScriptConfig,
+    vue: vueConfig,
+
+    default: [
+      ...baseConfig,
+      ...stylisticJsConfig,
+      ...typeScriptConfig,
+      ...stylisticTsConfig,
+      ...stylisticPlusConfig
+    ]
+  }
+}
