117c23559e03588fc13babc722b62ea5dc205b6b
Roman Nuritdinov (Ky6uk) <github@woof.slmail.me>
2025-01-15 11:22:02 +0200

    🚀 Upgrade dependencies

--- configs/typescript.js
+++ configs/typescript.js
@@ -139,6 +139,7 @@ export default tsEslint.config({
     '@typescript-eslint/no-meaningless-void-operator': 'error',
     '@typescript-eslint/no-misused-new': 'error',
     '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/no-misused-spread': 'error',
     '@typescript-eslint/no-namespace': 'error',
     '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
     '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
